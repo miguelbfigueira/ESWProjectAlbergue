9b43ab081c6cc847e5ddf41bae27134ea45e500e
Marcos Letras <marcos_let@hotmail.com>
2018-11-27 22:19:04 +0000

    E-Mail

--- ESWProjectAlbergue/Services/EMailSender.cs
+++ ESWProjectAlbergue/Services/EMailSender.cs
@@ -1,16 +1,49 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
+﻿using System.Net;
+using System.Net.Mail;
+using System.Text;
 using System.Threading.Tasks;
 using Microsoft.AspNetCore.Identity.UI.Services;
 
-namespace MediatecaEst.Services
+namespace ESWProjectAlbergue.Services
 {
     public class EmailSender : IEmailSender
     {
-        public Task SendEmailAsync(string email, string subject, string message)
+        public Task SendEmailAsync(string strToEmail, string strSubject, string strBody)
         {
+            SmtpClient smtpClient = new SmtpClient("smtp.gmail.com");
+            smtpClient.Credentials = new NetworkCredential("quintamiaoalbergue@gmail.com", "SWAlbergue5");
+            smtpClient.UseDefaultCredentials = false;
+            smtpClient.EnableSsl = true;
+
+            MailMessage mail = new MailMessage
+            {
+                From = new MailAddress("quintamiaoalbergue@gmail.com")
+            };
+            mail.To.Add(strToEmail);
+            mail.Body = strBody;
+            mail.Subject = strSubject;
+            mail.IsBodyHtml = true;
+
+            smtpClient.Send(mail);
             return Task.CompletedTask;
+
+        }
+
+        public async Task SendRegistrationEmail(string strEmailStudent)
+        {
+            string strSubject = "[QUINTA-DO-MIAO] Registo na Quinta do Miao";
+
+
+            var strbBody = new StringBuilder();
+            strbBody.AppendLine("Caro utilizador,<br><br>");
+            strbBody.AppendFormat(@"O seu pedido de registo na plataforma da Quinta do Mião foi aprovado.<br><br>");
+
+
+            strbBody.AppendLine("Cumprimentos, <br> A Equipa da Quinta do Miao.");
+            await SendEmailAsync(strEmailStudent, strSubject, strbBody.ToString());
         }
     }
 }
+
+    
+
