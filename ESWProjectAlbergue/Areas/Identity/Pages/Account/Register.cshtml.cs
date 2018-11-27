08b5ca8a17ab2967769784f57858392890eb3bf9
Marcos Letras <marcos_let@hotmail.com>
2018-11-27 23:49:42 +0000

    Commit

--- ESWProjectAlbergue/Areas/Identity/Pages/Account/Register.cshtml.cs
+++ ESWProjectAlbergue/Areas/Identity/Pages/Account/Register.cshtml.cs
@@ -12,6 +12,7 @@ using Microsoft.AspNetCore.Mvc.RazorPages;
 using Microsoft.Extensions.Logging;
 
 namespace ESWProjectAlbergue.Areas.Identity.Pages.Account
+
 {
     [AllowAnonymous]
     public class RegisterModel : PageModel
