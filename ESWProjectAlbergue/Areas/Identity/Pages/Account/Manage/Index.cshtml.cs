e88dda495d90a49791ceed7d8e8bbf535d8cd917
miguelbfigueira <miguelbfigueira@users.noreply.github.com>
2018-12-04 22:25:01 +0000

    Alterar dados

--- ESWProjectAlbergue/Areas/Identity/Pages/Account/Manage/Index.cshtml.cs
+++ ESWProjectAlbergue/Areas/Identity/Pages/Account/Manage/Index.cshtml.cs
@@ -139,14 +139,14 @@ namespace ESWProjectAlbergue.Areas.Identity.Pages.Account.Manage
                 Name = Input.Name,
                 Address = Input.Address,
                 BirthDate = Input.BirthDate,
-                PostalCode = Input.PostalCode,
+               
                
             };
             
             user.Name = Input.Name;
             user.Address = Input.Address;
             user.BirthDate = Input.BirthDate;
-            user.Postalcode = Input.PostalCode;
+           // user.Postalcode = Input.PostalCode;
 
             await _userManager.UpdateAsync(user);
 
