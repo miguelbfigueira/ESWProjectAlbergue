1cf838057c1082d85a15daad0855e459d93e6c0f
Valada345 <valada345@gmail.com>
2018-12-04 21:41:06 +0000

    Sem o codigo postal depois limpo a bosta que fiz.

--- ESWProjectAlbergue/Areas/Identity/Pages/Account/Register.cshtml.cs
+++ ESWProjectAlbergue/Areas/Identity/Pages/Account/Register.cshtml.cs
@@ -53,11 +53,13 @@ namespace ESWProjectAlbergue.Areas.Identity.Pages.Account
             [Display(Name = "Morada")]
             public string Address { get; set; }
 
+            /*
             [Required(ErrorMessage = "Insira o seu código postal.")]
             [Display(Name = "Código Postal")]
             [DataType(DataType.PostalCode)]
             [Range(0, int.MaxValue, ErrorMessage = "Please enter valid integer Number")]
             public string PostalCode { get; set; }
+            */
 
             [Required]
             [Display(Name = "Data De Nascimento")]
@@ -85,7 +87,7 @@ namespace ESWProjectAlbergue.Areas.Identity.Pages.Account
             returnUrl = returnUrl ?? Url.Content("~/");
             if (ModelState.IsValid)
             {
-                var user = new ApplicationUser { UserName = Input.Email, Email = Input.Email, Address = Input.Address, Postalcode = Input.PostalCode, BirthDate = Input.BirthDate, Name = Input.Name};
+                var user = new ApplicationUser { UserName = Input.Email, Email = Input.Email, Address = Input.Address/*, Postalcode = Input.PostalCode*/, BirthDate = Input.BirthDate, Name = Input.Name};
                 var result = await _userManager.CreateAsync(user, Input.Password);
                 if (result.Succeeded)
                 {
