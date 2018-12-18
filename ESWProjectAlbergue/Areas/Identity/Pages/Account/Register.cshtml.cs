5432cc770a4e9ae1c50d8439c86bceef5b927c2e
Marcos Letras <marcos_let@hotmail.com>
2018-12-18 17:29:52 +0000

    A puxar

--- ESWProjectAlbergue/Areas/Identity/Pages/Account/Register.cshtml.cs
+++ ESWProjectAlbergue/Areas/Identity/Pages/Account/Register.cshtml.cs
@@ -92,6 +92,7 @@ namespace ESWProjectAlbergue.Areas.Identity.Pages.Account
                 var result = await _userManager.CreateAsync(user, Input.Password);
                 if (result.Succeeded)
                 {
+                    await _userManager.AddToRoleAsync(user, "users");
                     _logger.LogInformation("User created a new account with password.");
 
                     var code = await _userManager.GenerateEmailConfirmationTokenAsync(user);
