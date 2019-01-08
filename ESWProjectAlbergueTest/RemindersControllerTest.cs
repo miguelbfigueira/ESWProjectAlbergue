282848aff04f497e05bdb08024170d82fea4f6ea
Valada345 <valada345@gmail.com>
2019-01-08 16:19:40 +0000

    Boa sorte Miguel

--- ESWProjectAlbergueTest/RemindersControllerTest.cs
+++ ESWProjectAlbergueTest/RemindersControllerTest.cs
@@ -1,12 +1,49 @@
-﻿using System;
+﻿using ESWProjectAlbergue.Controllers;
+using ESWProjectAlbergue.Models;
+using Microsoft.AspNetCore.Identity;
+using Microsoft.AspNetCore.Identity.UI.Services;
+using Xunit;
+using System;
 using System.Collections.Generic;
+using System.Linq;
 using System.Text;
-using ESWProjectAlbergue.Controllers;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Identity;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.Rendering;
+using Microsoft.Data.Sqlite;
+using Microsoft.EntityFrameworkCore;
 
 namespace ESWProjectAlbergueTest
 {
-    class RemindersControllerTest
+    public class RemindersControllerTest
     {
-        
+        public ESWProjectAlbergueContext DbContext { get; private set; }
+
+        private readonly UserManager<ApplicationUser> _userManager;
+        private readonly IEmailSender _emailSender;
+
+        public RemindersControllerTest()
+        {
+            var connection = new SqliteConnection("DataSource=:memory:");
+            connection.Open();
+            var options = new DbContextOptionsBuilder<ESWProjectAlbergueContext>()
+                    .UseSqlite(connection)
+                    .Options;
+            DbContext = new ApplicationDbContext(options);
+
+
+            DbContext.Database.EnsureCreated();
+        }
+
+        [Fact]
+        public void Index_ReturnsViewResult()
+        {
+            var controller = new RemindersController(DbContext, _userManager, _emailSender);
+
+            var result = controller.Index();
+
+            var viewResult = Assert.IsType<ViewResult>(result);
+        }
+
     }
-}
