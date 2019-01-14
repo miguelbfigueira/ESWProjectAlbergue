55659b6d7a2a51b02f0508760b51dc3c5766f598
Mariana Luciano <mariana.luciano2@hotmail.com>
2019-01-14 19:08:19 +0000

    Animais

---
+++ ESWProjectAlbergue/Models/EnumBreedType.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace ESWProjectAlbergue.Models
+{
+    public enum EnumBreedType
+    {
+        [Display(Name = "Husky")]
+        HUSKY,  
+   [Display(Name = "Beagle")]
+        BEAGLE,  
+   [Display(Name = "Great Dane")]
+        GREATDANE,
+   [Display(Name = "Outra")]
+        OTHER,
+
+    }
+    
+}
