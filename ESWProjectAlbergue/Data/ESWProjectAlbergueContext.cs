f67bba030ac5fd78bc63380d0e66d4dfc04cbe03
miguelbfigueira <miguelbfigueira@users.noreply.github.com>
2019-01-21 14:55:52 +0000

    revert

--- ESWProjectAlbergue/Data/ESWProjectAlbergueContext.cs
+++ ESWProjectAlbergue/Data/ESWProjectAlbergueContext.cs
@@ -18,9 +18,6 @@ namespace ESWProjectAlbergue.Models
    
         public DbSet<ESWProjectAlbergue.Models.ApplicationUser> User { get; set; }
         public DbSet<ESWProjectAlbergue.Models.Reminder> Reminder { get; set; }
-        public DbSet<ESWProjectAlbergue.Models.MainAnimal> MainAnimal { get; set; }
-        public DbSet<ESWProjectAlbergue.Models.ABreed> ABreed { get; set; }
-        public DbSet<ESWProjectAlbergue.Models.Visit> Visit { get; set; }
     }
 }
 
--- ESWProjectAlbergue/Migrations/20190114183751_animals.Designer.cs
+++ ESWProjectAlbergue/Migrations/20190114183751_animals.Designer.cs
@@ -10,8 +10,8 @@ using Microsoft.EntityFrameworkCore.Storage.ValueConversion;
 namespace ESWProjectAlbergue.Migrations
 {
     [DbContext(typeof(ESWProjectAlbergueContext))]
-    [Migration("20190114194511_5")]
-    partial class _5
+    [Migration("20190114183751_animals")]
+    partial class animals
     {
         protected override void BuildTargetModel(ModelBuilder modelBuilder)
         {
@@ -20,6 +20,37 @@ namespace ESWProjectAlbergue.Migrations
                 .HasAnnotation("ProductVersion", "2.1.4-rtm-31024")
                 .HasAnnotation("Relational:MaxIdentifierLength", 128)
                 .HasAnnotation("SqlServer:ValueGenerationStrategy", SqlServerValueGenerationStrategy.IdentityColumn);
+
+            modelBuilder.Entity("ESWProjectAlbergue.Models.Animal", b =>
+                {
+                    b.Property<int>("Id")
+                        .ValueGeneratedOnAdd()
+                        .HasAnnotation("SqlServer:ValueGenerationStrategy", SqlServerValueGenerationStrategy.IdentityColumn);
+
+                    b.Property<int>("AgeType");
+
+                    b.Property<int>("AnimalType");
+
+                    b.Property<int>("BehaviorType");
+
+                    b.Property<DateTime>("BirthDate");
+
+                    b.Property<int>("Breed");
+
+                    b.Property<string>("Description");
+
+                    b.Property<int>("FurType");
+
+                    b.Property<int>("Gender");
+
+                    b.Property<string>("Name");
+
+                    b.Property<int>("SizeType");
+
+                    b.HasKey("Id");
+
+                    b.ToTable("Animal");
+                });
 
             modelBuilder.Entity("ESWProjectAlbergue.Models.ApplicationUser", b =>
                 {
@@ -115,25 +146,6 @@ namespace ESWProjectAlbergue.Migrations
                     b.HasIndex("UserReminderIdId");
 
                     b.ToTable("Reminder");
-                });
-
-            modelBuilder.Entity("ESWProjectAlbergue.Models.Visit", b =>
-                {
-                    b.Property<int>("VisitId")
-                        .ValueGeneratedOnAdd()
-                        .HasAnnotation("SqlServer:ValueGenerationStrategy", SqlServerValueGenerationStrategy.IdentityColumn);
-
-                    b.Property<DateTime>("Date");
-
-                    b.Property<string>("Description");
-
-                    b.Property<string>("UserToVisitId");
-
-                    b.HasKey("VisitId");
-
-                    b.HasIndex("UserToVisitId");
-
-                    b.ToTable("Visit");
                 });
 
             modelBuilder.Entity("Microsoft.AspNetCore.Identity.IdentityRole", b =>
@@ -259,13 +271,6 @@ namespace ESWProjectAlbergue.Migrations
                     b.HasOne("ESWProjectAlbergue.Models.ApplicationUser", "UserReminderId")
                         .WithMany()
                         .HasForeignKey("UserReminderIdId");
-                });
-
-            modelBuilder.Entity("ESWProjectAlbergue.Models.Visit", b =>
-                {
-                    b.HasOne("ESWProjectAlbergue.Models.ApplicationUser", "UserToVisit")
-                        .WithMany()
-                        .HasForeignKey("UserToVisitId");
                 });
 
             modelBuilder.Entity("Microsoft.AspNetCore.Identity.IdentityRoleClaim<string>", b =>
