85723f393296d39940ffb1d55292eafca5585ac6
Mariana Luciano <mariana.luciano2@hotmail.com>
2019-02-07 23:40:42 +0000

    CSS

--- ESWProjectAlbergue/Models/Reminder.cs
+++ ESWProjectAlbergue/Models/Reminder.cs
@@ -24,7 +24,7 @@ namespace ESWProjectAlbergue.Models
         public string Title { get; set; }
         [DisplayName("Descrição")]
         public string Description { get; set; }
-        [DisplayName("É um evento")]
+        [DisplayName("É um evento?")]
         public bool IsEvent { get; set; }
         [DisplayName("Feito?")]
         public bool IsDone { get; set; }
